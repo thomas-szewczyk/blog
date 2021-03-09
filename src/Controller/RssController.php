fb3eab344b7b2ebde90b252b11a241aff61ecc2d
Tomek <thom.szewczyk@gmail.com>
2021-03-09 13:14:09 +0100

    - added a function which loads the xml file with the rs field

--- src/Controller/RssController.php
+++ src/Controller/RssController.php
@@ -5,21 +5,32 @@ namespace App\Controller;
 use App\Repository\PostRepository;
 use App\RSS\RSSFeed;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class RssController
+ * @package App\Controller
+ * @Route("/", name="rss_")
+ */
 class RssController extends AbstractController
 {
     /**
-     * @Route("/rss", name="rss")
+     * @Route("/rss", name="generate")
      * @param PostRepository $postRepository
+     * @param Request $request
      * @return Response
      */
-    public function index(PostRepository $postRepository): Response
+    public function generateRss(PostRepository $postRepository, Request $request): Response
     {
-        RSSFeed::generateRssFeed($postRepository->findAll());
+        $uri = $request->getBaseUrl();
+
+
+        RSSFeed::generateRssFeed($postRepository->findAllPostsAsEntities(), $uri);
         return $this->render('rss/index.html.twig', [
             'controller_name' => 'RssController',
         ]);
     }
+
 }
